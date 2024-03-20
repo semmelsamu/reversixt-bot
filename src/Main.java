0ba266540162f19cec6df342e07bf4d1ef73bad6
Samuel Kroiß <semmikroiss@gmail.com>
2024-03-20 14:40:14 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    private static final String VERSION = "0.1";
+
+    public static void main(String[] args) {
+        System.out.println("revxt-ss24-g04 v" + VERSION);
+    }
+}
