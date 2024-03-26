9ce834c5d7c12cb3698db45df4656a6a6cdecdb0
Samuel Kroiß <semmikroiss@gmail.com>
2024-03-26 22:35:02 +0100

    Change factory to use linked list instead of array

--- src/game/Game.java
+++ src/game/Game.java
@@ -4,6 +4,8 @@ import map.Board;
 import util.File;
 import util.Logger;
 
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Game {
@@ -49,6 +51,7 @@ public class Game {
      * Create a new game.
      */
     public Game(int initialPlayers, int initialOverwriteStones, int initialBombs, int bombRadius, Board board) {
+        Logger.log("Creating game");
         this.initialPlayers = initialPlayers;
         this.initialOverwriteStones = initialOverwriteStones;
         this.initialBombs = initialBombs;
@@ -67,29 +70,27 @@ public class Game {
      * @param lines A list of lines, where each line is trimmed and all lowercase (normalized).
      * @return The new game.
      */
-    private static Game createFromLines(List<String> lines) {
+    private static Game createFromLines(LinkedList<String> lines) {
 
         Logger.log("Creating game from lines");
 
-        // Splitting game and board lines
-        List<String> gameLines = lines.subList(0, 3);
-        List<String> boardLines = lines.subList(3, lines.size());
+        // We unshift line for line and parse it
 
         // Parsing initial player data
-        int initialPlayers = Integer.parseInt(gameLines.get(0));
+        int initialPlayers = Integer.parseInt(lines.remove(0));
         Logger.debug("Initial players: " + initialPlayers);
-        int initialOverwriteStones = Integer.parseInt(gameLines.get(1));
+        int initialOverwriteStones = Integer.parseInt(lines.remove(0));
         Logger.debug("Initial overwrite stones: " + initialOverwriteStones);
 
         // Parsing initial bomb data
-        String[] bombs = gameLines.get(2).split((" "));
+        String[] bombs = lines.remove(0).split((" "));
         int initialBombs = Integer.parseInt(bombs[0]);
         Logger.debug("Initial bombs: " + initialBombs);
         int bombRadius = Integer.parseInt(bombs[1]);
         Logger.debug("Bomb radius: " + bombRadius);
 
-        // Creating board
-        Board board = Board.createFromLines(boardLines);
+        // Create board from remaining lines
+        Board board = Board.createFromLines(lines);
 
         // Creating game
         return new Game(initialPlayers, initialOverwriteStones, initialBombs, bombRadius, board);
@@ -107,7 +108,7 @@ public class Game {
             lines[i] = lines[i].trim().toLowerCase();
         }
 
-        return createFromLines(List.of(lines));
+        return createFromLines(new LinkedList<>(Arrays.asList(lines)));
     }
 
     public static Game createFromFile(String filename) {
