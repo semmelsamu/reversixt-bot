58a290da1816d83d1c842b3109b7bc39b144de4d
Samuel Kroiß <semmikroiss@gmail.com>
2024-04-02 18:04:52 +0200

    Add high level pseudocode for exercise submission

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 import game.Game;
+import test.MapReadTest;
+import test.Test;
 import util.Logger;
 
 public class Main {
 
-    private static final String VERSION = "0.1";
+    private static final String VERSION = "0.2";
 
     public static void main(String[] args) {
         Logger.NAME = "revxt-ss24-g04-v" + VERSION;
@@ -13,8 +15,16 @@ public class Main {
 
         Logger.log("Starting");
 
+        // Specify map to read and load map
         Game game = Game.createFromFile("maps/initialMaps/window.map");
 
+        Logger.log(game.getValidMovesForCurrentPlayer().toString());
+
+        // Input
+
+        // Execute
+
+        // Print error if not valid or calculate and print new game if valid
         Logger.log(game.toString());
     }
 }
