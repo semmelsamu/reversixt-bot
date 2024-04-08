f04af4528136aa9968d60de7a9386aa76a3046ce
Samuel Kroiß <semmikroiss@gmail.com>
2024-04-08 23:57:28 +0200

    Add Playground class

---
+++ src/test/Playground.java
@@ -0,0 +1,15 @@
+package test;
+
+import util.Logger;
+
+public class Playground {
+
+    public static void main(String[] args) {
+        Logger.get().name = "Playground";
+        Logger.get().log("This is the Playground. Test your code here. It won't be tracked in version control.");
+
+        // Here you go...
+
+    }
+
+}
