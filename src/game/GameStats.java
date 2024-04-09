41dbe3296ae2f9487d33b46afc1231b785123f32
Samuel Kroiß <semmikroiss@gmail.com>
2024-04-09 10:05:02 +0200

    Created GameStats class

---
+++ src/game/GameStats.java
@@ -0,0 +1,13 @@
+package game;
+
+import board.Tile;
+import board.TileValue;
+
+import java.util.List;
+import java.util.Map;
+
+public class GameStats {
+
+    Map<TileValue, List<Tile>> tilesWithValue;
+
+}
