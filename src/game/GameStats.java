d4fe020a25a4e1b53109f95244148e09ca858a77
Samuel Kroiß <semmikroiss@gmail.com>
2024-04-09 10:29:38 +0200

    Add Game Stats constructor

--- src/game/GameStats.java
+++ src/game/GameStats.java
@@ -2,12 +2,26 @@ package game;
 
 import board.Tile;
 import board.TileValue;
+import util.Logger;
 
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 public class GameStats {
 
     Map<TileValue, List<Tile>> tilesWithValue;
+
+    public GameStats(Game game) {
+
+        tilesWithValue = new HashMap<>();
+
+        for(TileValue value : TileValue.values()) {
+            tilesWithValue.put(value, new LinkedList<>(game.getAllTilesWithValue(value)));
+        }
+
+        Logger.get().debug(tilesWithValue.toString());
+    }
 
 }
