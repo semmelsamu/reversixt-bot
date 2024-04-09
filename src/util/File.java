64f5b2d961ad01f0909a7cb2d680d90b6dbf519a
Ludwig <schmidtludwig8@gmail.com>
2024-04-09 10:04:28 +0200

    Add change to File.getAllMaps that maps are returned sorted

--- src/util/File.java
+++ src/util/File.java
@@ -5,6 +5,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -38,6 +40,7 @@ public class File {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        Collections.sort(mapFiles);
         return mapFiles;
     }
 }
