030be9ca2926afd7b78deca3cbd2f97bb2c0c691
Samuel Kroiß <semmikroiss@gmail.com>
2024-04-10 14:52:00 +0200

    Add replaceTileAtCoordinates

--- src/game/GameStats.java
+++ src/game/GameStats.java
@@ -25,7 +25,18 @@ public class GameStats {
         return tilesWithValue.get(tile);
     }
 
+    public void replaceTileAtCoordinates(Coordinates coordinates, Tile tile) {
+        tilesWithValue.get(findKeyByValue(tilesWithValue, coordinates)).remove(coordinates);
+        tilesWithValue.get(tile).add(coordinates);
+    }
 
-
-
+    // TODO: Performance
+    public static <K, V> K findKeyByValue(Map<K, List<V>> map, V gesuchtesElement) {
+        for (Map.Entry<K, List<V>> eintrag : map.entrySet()) {
+            if (eintrag.getValue().contains(gesuchtesElement)) {
+                return eintrag.getKey(); // Der Key wird zurückgegeben, wenn das Element gefunden wurde
+            }
+        }
+        return null; // null wird zurückgegeben, wenn das Element nicht gefunden wurde
+    }
 }
