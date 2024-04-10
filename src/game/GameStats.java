e02eef8fa167e0e26cc64e12eb5aca689f932494
Samuel Kroiß <semmikroiss@gmail.com>
2024-04-10 14:40:01 +0200

    Remove outdated game stats functions

--- src/game/GameStats.java
+++ src/game/GameStats.java
@@ -18,33 +18,14 @@ public class GameStats {
             tilesWithValue.put(tile, new LinkedList<>(game.getAllCoordinatesWhereTileIs(tile)));
         }
 
-        Logger.get().debug(tilesWithValueToString());
+        Logger.get().debug(());
     }
 
     public List<Coordinates> getAllCoordinatesWhereTileIs(Tile tile) {
         return tilesWithValue.get(tile);
     }
 
-    public void removeTile(Tile tile) {
-        tilesWithValue.get(tile.getValue()).remove(tile);
-    }
 
-    public void addTile(Tile tile) {
-        tilesWithValue.get(tile.getValue()).add(tile);
-    }
 
-    public String tilesWithValueToString() {
-        StringBuilder result = new StringBuilder("Tiles\n");
-
-        for(var key : tilesWithValue.keySet().stream().sorted().toArray()) {
-            result.append(key.toString()).append(": ");
-            for(var value : tilesWithValue.get(key)) {
-                result.append(value.getPosition()).append(", ");
-            }
-            result.append("\n");
-        }
-
-        return result.toString();
-    }
 
 }
