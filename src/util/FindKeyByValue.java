96ca70a5c8f6d58b1d109a318075de3bd0e3a4b4
Samuel Kroiß <semmikroiss@gmail.com>
2024-04-10 17:51:40 +0200

    Remove ugly comment

--- src/util/FindKeyByValue.java
+++ src/util/FindKeyByValue.java
@@ -8,8 +8,7 @@ public class FindKeyByValue {
     public static <K, V> K findKeyByValue(Map<K, List<V>> map, V gesuchtesElement) {
         for (Map.Entry<K, List<V>> eintrag : map.entrySet()) {
             if (eintrag.getValue().contains(gesuchtesElement)) {
-                return eintrag.getKey(); // Der Key wird zurückgegeben, wenn das Element gefunden
-                // wurde
+                return eintrag.getKey();
             }
         }
         return null; // null wird zurückgegeben, wenn das Element nicht gefunden wurde
