ced7a8565d44202cfb26d079a9f755ffbbfbe4fa
Samuel Kroiß <semmikroiss@gmail.com>
2024-04-10 14:53:11 +0200

    Remove debug message in game stats constructor

--- src/game/GameStats.java
+++ src/game/GameStats.java
@@ -11,14 +11,10 @@ public class GameStats {
     Map<Tile, List<Coordinates>> tilesWithValue;
 
     public GameStats(Game game) {
-
         tilesWithValue = new HashMap<>();
-
         for(Tile tile : Tile.values()) {
             tilesWithValue.put(tile, new LinkedList<>(game.getAllCoordinatesWhereTileIs(tile)));
         }
-
-        Logger.get().debug(());
     }
 
     public List<Coordinates> getAllCoordinatesWhereTileIs(Tile tile) {
