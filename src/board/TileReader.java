5f39ec36b86a8b07a103eda4dc2c63f7089bb88b
Samuel Kroiß <semmikroiss@gmail.com>
2024-04-10 14:21:29 +0200

    Add TileReader class

---
+++ src/board/TileReader.java
@@ -0,0 +1,49 @@
+package board;
+
+import game.Game;
+
+public class TileReader {
+
+    /**
+     * The game object this tile reader operates on.
+     */
+    private final Game game;
+
+    /**
+     * The coordinates of the current tile.
+     */
+    private Coordinates coordinates;
+
+    /**
+     * The direction the reader moves when next() is called.
+     */
+    private Direction direction;
+
+    public TileReader(Game game, Coordinates coordinates, Direction direction) {
+        this.game = game;
+        this.coordinates = coordinates;
+        this.direction = direction;
+    }
+
+    /**
+     * Return the tile which the reader currently points at.
+     */
+    public Tile getTile() {
+        return Tile.EMPTY; // TODO
+    }
+
+    /**
+     * Return if the reader has a neighbour in the current direction.
+     */
+    public boolean hasNext() {
+        return true; // TODO
+    }
+
+    /**
+     * Move the reader one in the current direction.
+     */
+    public void next() {
+        // TODO
+    }
+
+}
