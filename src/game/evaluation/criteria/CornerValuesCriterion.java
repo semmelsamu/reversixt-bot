029bdf5820cf77b0266a44e8b1e8718ce6276f0a
Maximilian Strauß <maximilian.strauss03@gmail.com>
2024-04-14 19:10:27 +0200

    Adds CornerValuesCriterion

--- src/game/evaluation/criteria/CornerValuesCriterion.java
+++ src/game/evaluation/criteria/CornerValuesCriterion.java
@@ -1,9 +1,18 @@
 package game.evaluation.criteria;
 
+import board.Coordinates;
+import board.Direction;
+import board.TileReader;
 import game.Game;
 import game.evaluation.AbstractRating;
+import game.evaluation.MapTileRating;
 import game.evaluation.RatingType;
 
+import java.util.List;
+
+/**
+ * Rates every tile, tiles with fewer neighbours are better
+ */
 public class CornerValuesCriterion extends AbstractRating {
 
     public CornerValuesCriterion(Game game) {
@@ -12,6 +21,18 @@ public class CornerValuesCriterion extends AbstractRating {
 
     @Override
     public void evaluateByCriterion() {
-
+        List<Coordinates> playerTiles = getGame().getAllCoordinatesWhereTileIs(
+                getGame().getCurrentPlayer().getPlayerValue());
+        for(Coordinates tile : playerTiles) {
+            int availableNeighbours = 0;
+            for (Direction direction : Direction.values()) {
+                TileReader tileReader = new TileReader(getGame(), tile, direction);
+                if (!tileReader.hasNext()) {
+                    continue;
+                }
+                availableNeighbours++;
+            }
+            addPlayerRatingByCriterion(new MapTileRating(tile, Math.abs(8 - availableNeighbours)));
+        }
     }
 }
