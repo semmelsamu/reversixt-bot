e42739c977563b1a68ca5f56f8254d1ccb93ab2e
Samuel Kroiß <semmikroiss@gmail.com>
2024-04-17 17:43:52 +0200

    Fix getAllMaps and readFile

--- src/main/java/util/File.java
+++ src/main/java/util/File.java
@@ -1,14 +1,12 @@
 package util;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -30,7 +28,7 @@ public class File {
      */
     public static String readFile(String filename) {
         try {
-            return new String(Files.readAllBytes(Paths.get(filename.replaceFirst("^/[A-Za-z]:", ""))));
+            return new String(Files.readAllBytes(Paths.get(filename)));
         } catch (Exception e) {
             throw new RuntimeException("Error reading file " + filename);
         }
@@ -41,17 +39,11 @@ public class File {
      */
     public static List<String> getAllMaps() {
         List<String> mapFiles = new ArrayList<>();
-        try {
-            ClassLoader classLoader = File.class.getClassLoader();
-            Path mapsPath =
-                    Paths.get(Objects.requireNonNull(classLoader.getResource("maps")).toURI());
-            try (Stream<Path> paths = Files.walk(mapsPath)) {
-                mapFiles = paths.filter(Files::isRegularFile)
-                        .map(Path::toString).filter(string -> string.endsWith(".map"))
-                        .collect(Collectors.toList());
-            }
-        } catch (URISyntaxException | IOException e) {
-            Logger.get().fatal(e.getMessage());
+        try (Stream<Path> paths = Files.walk(Paths.get("maps"))) {
+            mapFiles = paths.filter(Files::isRegularFile).map(Path::toString)
+                    .filter(string -> string.endsWith(".map")).collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         Collections.sort(mapFiles);
         return mapFiles;
