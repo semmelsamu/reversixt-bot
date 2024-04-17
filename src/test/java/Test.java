fd910723d91fb1e2a8f0a9c0bfb7c2414ad5975a
Samuel Kroiß <semmikroiss@gmail.com>
2024-04-17 18:28:51 +0200

    Started adding all test to main test

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,7 +1,11 @@
+import tests.MapReadingTest;
+import tests.MoveExecutionTest;
+
 public class Test {
 
     public static void main(String[] args) {
-
+        (new MapReadingTest()).testAllMaps();
+        (new MoveExecutionTest()).testEveryMove();
     }
 
 }
