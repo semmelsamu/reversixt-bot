7d20c6c38ae07323996fd7b06447bdffd14f9865
Samuel Kroiß <semmikroiss@gmail.com>
2024-04-17 21:28:41 +0200

    Rename MoveExecutionTest to MoveExecutionExceptionTest

--- src/test/java/MoveExecutionExceptionTest.java
+++ src/test/java/MoveExecutionExceptionTest.java
@@ -11,7 +11,7 @@ import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
-public class MoveExecutionTest {
+public class MoveExecutionExceptionTest {
 
     @Test
     public void testEveryMove() {
