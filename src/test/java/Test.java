5ade1b362314d345fb48cc724e98e0c66fab1396
Samuel Kroiß <semmikroiss@gmail.com>
2024-04-17 18:36:24 +0200

    Add logger priority to main tes

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,9 +1,15 @@
 import tests.MapReadingTest;
 import tests.MoveExecutionTest;
+import util.Logger;
+import util.TestLogger;
 
 public class Test {
 
     public static void main(String[] args) {
+
+        Logger.get().priority = 5;
+        TestLogger.get().priority = 2;
+
         (new MapReadingTest()).testAllMaps();
         (new MoveExecutionTest()).testEveryMove();
     }
