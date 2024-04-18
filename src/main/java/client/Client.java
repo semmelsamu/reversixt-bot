3bda2257d145659332af2ef859e9b8474407f817
Samuel Kroiß <semmikroiss@gmail.com>
2024-04-18 19:43:16 +0200

    Add Client interface

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,66 +1,17 @@
 package client;
 
-import util.Logger;
+import player.move.Move;
 
-import java.io.*;
-import java.net.*;
+public interface Client {
 
-/**
- * The client connects to the server. It waits for the server's messages, and reacts accordingly.
- */
-public class Client {
+    public int getGroupName();
 
-    private Socket socket;
-    private DataOutputStream out;
-    private DataInputStream in;
+    public void setMap();
 
-    public Client(String ip, int port) throws IOException {
-        socket = new Socket(ip, port);
-        out = new DataOutputStream(socket.getOutputStream());
-        in = new DataInputStream(socket.getInputStream());
-    }
+    public void setPlayer();
 
-    private void sendGroupNumber(int groupNumber) throws IOException {
-        out.writeByte(1); // Message type
-        out.writeInt(1); // Message length
-        out.writeByte(groupNumber);
-    }
+    public Move getMove();
 
-    public void run() throws IOException {
+    public void processMove();
 
-        while (!socket.isClosed()) {
-
-            int messageType = in.readByte();
-            int length = in.readInt();
-            byte[] message = new byte[length];
-            in.readFully(message);
-
-            switch (messageType) {
-                //   1: // Client sends group name
-                case 2: // Server sends map
-                    processGameField(new String(message));
-                    break;
-                case 3: // Server assigns player number
-                    processPlayerNumber(message[0]);
-                    break;
-                case 4: // Server requests move
-                    respondToMoveRequest();
-                    break;
-                //   5: // Client sends move
-                case 6: // Server sends move from other player
-                    processMoveNotification(message);
-                    break;
-                case 7: // Server disqualifies
-                    processDisqualification(message[0]);
-                    break;
-                case 8: // End of phase 1
-                    break;
-                case 9: // End of phase 2 - the end.
-                    processGameEnd();
-                    break;
-                default:
-                    Logger.get().warn("Unknown message type: " + messageType);
-            }
-        }
-    }
 }
