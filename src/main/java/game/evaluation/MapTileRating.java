6aa6fb91dc02ce1b8b1ea6f214dfcd108624e473
Ludwig <schmidtludwig8@gmail.com>
2024-04-19 20:34:04 +0200

    Add evaluation folder from branch evaluation

---
+++ src/main/java/game/evaluation/MapTileRating.java
@@ -0,0 +1,18 @@
+package game.evaluation;
+
+import board.Coordinates;
+
+/**
+ * Container which contains coordinates and values. Values are added to the player rating.
+ * Coordinates can be used to represent a board structure
+ * TODO: may be unnecessary, if we don't need a map
+ *
+ * @param coordinates
+ * @param value
+ */
+public record MapTileRating(
+        Coordinates coordinates,
+        int value
+) {
+
+}
