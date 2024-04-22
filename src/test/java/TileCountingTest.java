b29f0ac1294dbe272f8368b7b86390e759628422
Maximilian Strauß <maximilian.strauss@st.oth-regensburg.de>
2024-04-22 09:23:18 +0200

    Updates Tile Counting test

--- src/test/java/TileCountingTest.java
+++ src/test/java/TileCountingTest.java
@@ -12,47 +12,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TileCountingTest {
 
     @Test
-    public void testTileCount() {
-        Map<String, Map<Tile, Integer>> mapsAndTileCounts = new HashMap<>();
+    public void countWindowMapPlayersTiles_test(){
+        Game game = GameFactory.createFromFile("maps/initialMaps/window.map");
 
-        String windowMap = "maps/initialMaps/window.map";
-        mapsAndTileCounts.put(windowMap, createTileCountMap(4));
-        String checkerboardMap = "maps/initialMaps/checkerboard.map";
-        mapsAndTileCounts.put(checkerboardMap, createTileCountMap(8));
-
-        int errors = 0;
-
-        for (var mapAndTileCounts : mapsAndTileCounts.entrySet()) {
-            String mapPath =
-                    mapAndTileCounts.getKey().equals(windowMap) ? windowMap : checkerboardMap;
-            Game game = GameFactory.createFromFile(mapPath);
-
-            for (var tileAndCount : mapAndTileCounts.getValue().entrySet()) {
-                int calculatedTileCount =
-                        game.getAllCoordinatesWhereTileIs(tileAndCount.getKey()).size();
-                int expectedTileCount = tileAndCount.getValue();
-
-                String message =
-                        "Map " + mapAndTileCounts.getKey() + " calculated " + calculatedTileCount +
-                                " tiles for player " + tileAndCount.getKey().character +
-                                " (expected " + expectedTileCount + ")";
-
-                if (calculatedTileCount != expectedTileCount) {
-                    TestLogger.get().error(message);
-                    errors++;
-                }
-            }
+        for (Tile allParticipatingPlayer : game.getAllParticipatingPlayers()) {
+            int calculatedTileCount =
+                    game.getAllCoordinatesWhereTileIs(allParticipatingPlayer).size();
+            assertEquals(4, calculatedTileCount, "Tile count for each player at 4");
         }
-
-        assertEquals(0, errors, "There were " + errors + " errors in tile count.");
     }
 
-    private Map<Tile, Integer> createTileCountMap(int count) {
-        Map<Tile, Integer> tileCountMap = new HashMap<>();
-        tileCountMap.put(Tile.PLAYER1, count);
-        tileCountMap.put(Tile.PLAYER2, count);
-        tileCountMap.put(Tile.PLAYER3, count);
-        tileCountMap.put(Tile.PLAYER4, count);
-        return tileCountMap;
+    @Test
+    public void countCheckerboardMapPlayersTiles_test(){
+        Game game = GameFactory.createFromFile("maps/initialMaps/checkerboard.map");
+
+        for (Tile allParticipatingPlayer : game.getAllParticipatingPlayers()) {
+            int calculatedTileCount =
+                    game.getAllCoordinatesWhereTileIs(allParticipatingPlayer).size();
+            assertEquals(8, calculatedTileCount, "Tile count for each player at 8");
+        }
     }
 }
