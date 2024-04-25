536fb7e0c7d2f4df751865e8b71589a4301eb04b
Ludwig <schmidtludwig8@gmail.com>
2024-04-25 15:03:12 +0200

    Comment out methods in GameEvaluator and add tileRatings

--- src/main/java/game/evaluation/GameEvaluator.java
+++ src/main/java/game/evaluation/GameEvaluator.java
@@ -1,10 +1,10 @@
 package game.evaluation;
 
+import board.*;
 import game.Game;
-import game.evaluation.criteria.*;
-import util.Logger;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -14,16 +14,20 @@ public class GameEvaluator {
 
     private int playerRating;
 
-    private Game game;
+    private final Game game;
+    private final Tile player;
+    private final int[][] tileRatings; //TODO: should be placed in a higher standing class, as it is valid for the whole game
 
     // needs to be list for sorting reason
     private List<AbstractRating> ratings;
 
-    public GameEvaluator(Game game) {
+    public GameEvaluator(Game game, Tile player) {
         this.game = game;
         this.playerRating = 0;
+        this.player = player;
+        tileRatings = calculateTileRatings();
         ratings = new ArrayList<>();
-        registerCriteria();
+        //registerCriteria();
     }
 
     /**
@@ -36,6 +40,55 @@ public class GameEvaluator {
         }
     }
 
+    /**
+     * Calls calculateParticularTileRating(x, y) for each coordinate
+     * @return Array of tileRatings
+     */
+    private int[][] calculateTileRatings() {
+        int width = game.getWidth();
+        int height = game.getHeight();
+        int[][] tileRatings = new int[height][width];
+        for(int i = 0; i < height; i++) {
+            for(int j = 0; j < width; j++) {
+                if(game.getTile(new Coordinates(j, i)) == Tile.WALL){
+                    tileRatings[i][j] = 0;
+                    continue;
+                }
+                tileRatings[i][j] = calculateParicularTileRating(j, i);
+            }
+        }
+        return tileRatings;
+    }
+
+    /**
+     * Default tile rating: 1
+     * Checks all 4 angles on, whether they are only open in one direction
+     * Such angles are valuable, as tiles can "attack" in the angle but can not be "attacked"
+     * 1 bonuspoint for every angle, that fullfills the criterion
+     * Rating for WALLS: 0
+     * @param x X-coordinate
+     * @param y Y-coordinate
+     * @return Tile rating as an Integer
+     */
+    private int calculateParicularTileRating(int x, int y){
+        int tileRating = 1;
+        Direction[] halfOfAllDirections = Arrays.copyOfRange(Direction.values(),0, 4);
+        for(Direction direction : halfOfAllDirections) {
+            TileReader tileReader = new TileReader(game, new Coordinates(x, y), direction);
+            TileReader oppositeDirectionTileReader =
+                    new TileReader(game, new Coordinates(x, y), direction.getOppositeDirection());
+            if(tileReader.hasNext() && !oppositeDirectionTileReader.hasNext()){
+                tileRating++;
+                continue;
+            }
+            if(!tileReader.hasNext() && oppositeDirectionTileReader.hasNext()){
+                tileRating++;
+            }
+        }
+        return tileRating;
+    }
+
+    /*
     private void registerCriteria() {
         ratings.add(new CornerValuesCriterion(game));
         ratings.add(new AmountOverwriteStonesCriterion(game));
@@ -48,7 +101,7 @@ public class GameEvaluator {
         // needs to be last one
         ratings.add(new InversionTileCriterion(game));
     }
-
+    */
     /**
      * Returns the list of {@link MapTileRating} by {@link RatingType}
      * //TODO: may be unnecessary
@@ -56,6 +109,7 @@ public class GameEvaluator {
      * @param ratingType different types of ratings
      * @return list or null if not type found
      */
+    /*
     public List<MapTileRating> getTileRatingsByRatingType(RatingType ratingType) {
         for (AbstractRating rating : ratings) {
             if (rating.getRatingType() == ratingType) {
@@ -65,6 +119,8 @@ public class GameEvaluator {
         return null;
     }
 
+     */
+    /*
     public int getPlayerRating() {
         return playerRating;
     }
@@ -73,9 +129,12 @@ public class GameEvaluator {
         ratings.removeIf(abstractRating -> abstractRating instanceof InversionTileCriterion);
     }
 
+     */
+
     /**
      * Prints the player ratings by type
      */
+    /*
     public void printRatings() {
         for (AbstractRating rating : ratings) {
             if (rating.getMapTileRatings().isEmpty()) {
@@ -89,4 +148,5 @@ public class GameEvaluator {
             }
         }
     }
+     */
 }
