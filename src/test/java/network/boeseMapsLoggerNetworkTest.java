4e78e2f5ad4328966062a8e8f06b2e6c85327383
Maximilian Strauß <maximilian.strauss03@gmail.com>
2024-04-25 15:58:13 +0200

    Fix logger bug, so every test has own logger spy

--- src/test/java/network/boeseMapsLoggerNetworkTest.java
+++ src/test/java/network/boeseMapsLoggerNetworkTest.java
@@ -2,8 +2,9 @@ package network;
 
 import clients.RandomMoveClient;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import util.Logger;
 import util.NetworkClientHelper;
 import util.NetworkServerHelper;
@@ -12,7 +13,8 @@ import java.io.IOException;
 import java.lang.reflect.Field;
 
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 public class boeseMapsLoggerNetworkTest {
 
@@ -20,14 +22,14 @@ public class boeseMapsLoggerNetworkTest {
 
     private NetworkServerHelper server;
 
-    @BeforeAll
-    public static void setUp() throws NoSuchFieldException, IllegalAccessException {
-        loggerSpy = spy(Logger.get());
+    @BeforeEach
+    public void setUp() throws NoSuchFieldException, IllegalAccessException {
+        loggerSpy = Mockito.mock(Logger.class);
 
         // Verwende Reflection, um das private Feld zu finden
         Field field = Logger.class.getDeclaredField("logger");
         field.setAccessible(true); // Mache das private Feld zugänglich
-        field.set(null, loggerSpy);
+        field.set(field, loggerSpy);
 
 
     }
@@ -111,11 +113,15 @@ public class boeseMapsLoggerNetworkTest {
     }
 
     @AfterEach
-    public void tearDown() {
+    public void tearDown() throws NoSuchFieldException, IllegalAccessException {
 
         server.stopServer();
 
         verify(loggerSpy, times(0)).error(anyString());
         verify(loggerSpy, times(0)).fatal(anyString());
+
+        Field instance = Logger.class.getDeclaredField("logger");
+        instance.setAccessible(true);
+        instance.set(null, null);
     }
 }
