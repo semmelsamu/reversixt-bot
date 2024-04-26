a002f5e71ce549f44be0d960c683a39c1fa22380
Samuel Kroiß <semmikroiss@gmail.com>
2024-04-27 00:39:10 +0200

    Add documentation

--- src/main/java/game/GamePhase.java
+++ src/main/java/game/GamePhase.java
@@ -1,11 +1,23 @@
 package game;
 
 /**
- * Represent the different phases, from setting tiles to the bomb phase
+ * Represent the different phases of the game.
  */
 public enum GamePhase {
 
+    /**
+     * The "tile setting" phase.
+     */
     PHASE_1,
+
+
+    /**
+     * The bomb phase.
+     */
     PHASE_2,
+
+    /**
+     * The end.
+     */
     END
 }
