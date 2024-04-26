9d8ff36aec5da5677aa112499fa1e5ec86ab0ade
Samuel Kroiß <semmikroiss@gmail.com>
2024-04-27 01:15:32 +0200

    Update main to run new exercise

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,20 @@
-import exercises.Exercise02;
-import exercises.Exercise04;
-import game.Game;
-import game.GameFactory;
+import clients.ParanoidClient;
+import exercises.Exercise05;
+import network.NetworkEventHandler;
 import util.CommandLineArguments;
 import util.Logger;
 
-import java.io.IOException;
-import java.util.Arrays;
-
 public class Main {
-
-    private static final String VERSION = "0.4";
-
     public static void main(String[] args) {
 
-        Logger.defaultPriority = 2;
+        Logger.defaultPriority = 3;
+        Logger.setPriority(NetworkEventHandler.class.getName(), 2);
+        Logger.setPriority(ParanoidClient.class.getName(), 0);
 
         var parsedArgs = CommandLineArguments.parse(args);
 
         // Launch whatever
-        Exercise04.abnahme(parsedArgs.getOrDefault("-i", "127.0.0.1"),
+        Exercise05.abnahme(parsedArgs.getOrDefault("-i", "127.0.0.1"),
                 Integer.parseInt(parsedArgs.getOrDefault("-p", "7777")));
     }
 }
--- src/main/java/exercises/Exercise05.java
+++ src/main/java/exercises/Exercise05.java
@@ -1,10 +1,11 @@
 package exercises;
 
+import clients.ParanoidClient;
 import clients.RandomMoveClient;
 import network.Launcher;
 public class Exercise05 {
 
     public static void abnahme(String ip, int port) {
-        Launcher.launchClientOnNetwork(new RandomMoveClient(), ip, port);
+        Launcher.launchClientOnNetwork(new ParanoidClient(), ip, port);
     }
 }
