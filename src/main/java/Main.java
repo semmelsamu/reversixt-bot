94629a2ba14f85094aa0f7d5f8d7468ab8bbd7fd
Samuel Kroiß <semmikroiss@gmail.com>
2024-04-29 18:03:12 +0200

    Add default logging level command line parameter option

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,12 +9,12 @@ import util.Logger;
 public class Main {
     public static void main(String[] args) {
 
-        Logger.defaultPriority = 3;
-        Logger.setPriority(NetworkEventHandler.class.getName(), 1);
-        Logger.setPriority(ParanoidClient.class.getName(), 0);
-        Logger.setPriority(Player.class.getName(), 2);
-
         var parsedArgs = CommandLineArguments.parse(args);
+
+        Logger.defaultPriority = Integer.parseInt(parsedArgs.getOrDefault("-l", "3"));
+        Logger.setPriority(NetworkEventHandler.class.getName(), 1);
+        Logger.setPriority(ParanoidClient.class.getName(), 1);
+        Logger.setPriority(Player.class.getName(), 2);
 
         // Launch whatever
         Exercise05.abnahme(parsedArgs.getOrDefault("-i", "127.0.0.1"),
