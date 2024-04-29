99febec5ffb8d3e41120796e0c0260817fcf0476
Samuel Kroiß <semmikroiss@gmail.com>
2024-04-29 16:36:12 +0200

    Comment out not yet refactored functions

--- src/test/java/util/MoveCalculatorHelper.java
+++ src/test/java/util/MoveCalculatorHelper.java
@@ -9,12 +9,12 @@ import player.move.Move;
 import java.util.Set;
 
 public class MoveCalculatorHelper {
-
+/*
     public static Set<Move> getAllValidMovesForPlayer(Game game, Tile player){
         return new MoveCalculator(game).getValidMovesForPlayer(player);
     }
 
     public static Move createDummyMove(Tile player, int x, int y){
         return new Move(player, new Coordinates(x, y));
-    }
+    }*/
 }
