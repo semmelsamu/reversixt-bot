9e819a6dd033d049ec7baa27553bba0c6dc4dc36
Samuel Kroiß <semmikroiss@gmail.com>
2024-04-30 12:27:04 +0200

    Add switch to select exercise

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,12 @@
 import clients.ParanoidClient;
+import exercises.Exercise01;
+import exercises.Exercise02;
+import exercises.Exercise04;
 import exercises.Exercise05;
 import game.Game;
 import game.MoveCalculator;
 import game.MoveExecutor;
+import network.NetworkClientAdapter;
 import network.NetworkEventHandler;
 import player.Player;
 import util.CommandLineArguments;
@@ -13,14 +17,22 @@ public class Main {
 
         var parsedArgs = CommandLineArguments.parse(args);
 
-        Logger.defaultPriority = Integer.parseInt(parsedArgs.getOrDefault("-l", "5"));
+        Logger.defaultPriority = Integer.parseInt(parsedArgs.getOrDefault("-l", "3"));
         Logger.setPriority(NetworkEventHandler.class.getName(), 1);
-        Logger.setPriority(ParanoidClient.class.getName(), 1);
-        Logger.setPriority(Game.class.getName(), 2);
+        Logger.setPriority(ParanoidClient.class.getName(), 0);
+        Logger.setPriority(NetworkClientAdapter.class.getName(), 1);
 
         // Launch whatever
-        Exercise05.abnahme(parsedArgs.getOrDefault("-i", "127.0.0.1"),
-                Integer.parseInt(parsedArgs.getOrDefault("-p", "7777")),
-                Integer.parseInt(parsedArgs.getOrDefault("-d", "3")));
+
+        switch (parsedArgs.getOrDefault("-e", "0")) {
+            case "1" -> Exercise01.aufgabe3();
+            case "2" -> Exercise02.aufgabe3();
+            case "4" -> Exercise04.abnahme(parsedArgs.getOrDefault("-i", "127.0.0.1"),
+                    Integer.parseInt(parsedArgs.getOrDefault("-p", "7777")));
+            default -> Exercise05.abnahme(parsedArgs.getOrDefault("-i", "127.0.0.1"),
+                    Integer.parseInt(parsedArgs.getOrDefault("-p", "7777")),
+                    Integer.parseInt(parsedArgs.getOrDefault("-d", "3")));
+        }
+
     }
 }
