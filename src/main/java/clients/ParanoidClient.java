5d3fcaedb18257053318c5e5460e96d70dc21df4
Samuel Kroiß <semmikroiss@gmail.com>
2024-05-07 18:02:39 +0200

    Fix bugs, update formatting

--- src/main/java/clients/ParanoidClient.java
+++ src/main/java/clients/ParanoidClient.java
@@ -27,51 +27,59 @@ public class ParanoidClient extends Client {
 
         Set<Move> possibleMoves = MoveCalculator.getValidMovesForPlayer(game, ME);
 
-        if(possibleMoves.isEmpty()) {
+        if (possibleMoves.isEmpty()) {
             throw new RuntimeException("Could not calculate any possible moves");
         }
+
+        logger.debug(possibleMoves.size() + " possible moves");
 
         int resultScore = Integer.MIN_VALUE;
         Move resultMove = null;
 
-        for(Move move : possibleMoves) {
+        for (Move move : possibleMoves) {
 
             Game clonedGame = game.clone();
             MoveExecutor.executeMove(clonedGame, move);
             int score = minmax(clonedGame, depthLimit);
 
-            if(score > resultScore) {
+            if (score > resultScore) {
                 resultScore = score;
                 resultMove = move;
             }
         }
+
+        logger.log("Responding with " + resultMove.getClass().getSimpleName() +
+                resultMove.getCoordinates() + " which has a score of " + resultScore);
 
         return resultMove;
     }
 
     private int minmax(Game game, int depth) {
 
-        if(depth == 0 || game.getPhase() != GamePhase.PHASE_1) {
+        if (depth == 0 || game.getPhase() != GamePhase.PHASE_1) {
             return GameEvaluator.evaluate(game, ME);
         }
 
-        boolean isMaximizer = game.getCurrentPlayerNumber() == ME;
+        int currentPlayerNumber = game.getCurrentPlayerNumber();
+
+        boolean isMaximizer = currentPlayerNumber == ME;
 
         int result = isMaximizer ? Integer.MIN_VALUE : Integer.MAX_VALUE;
 
-        for(Move move : MoveCalculator.getValidMovesForPlayer(game, ME)) {
+        for (Move move : MoveCalculator.getValidMovesForPlayer(game, currentPlayerNumber)) {
 
             Game clonedGame = game.clone();
-            MoveExecutor.executeMove(game, move);
+            MoveExecutor.executeMove(clonedGame, move);
             int score = minmax(clonedGame, depth - 1);
 
-            if(isMaximizer) {
-                if(score > result)
+            if (isMaximizer) {
+                if (score > result) {
                     result = score;
-            }
-            else {
-                if(score < result)
+                }
+            } else {
+                if (score < result) {
                     result = score;
+                }
             }
 
         }
