493a9d736a7cf603a0c1c070888092acc455aead
Maximilian Strauß <maximilian.strauss03@gmail.com>
2024-06-13 21:45:33 +0200

    Revert "Add area search to MoveCalculator"
    This reverts commit ef75dcab25edcb0d138f47d1c98f80f918b2a2df.

--- src/main/java/game/MoveCalculator.java
+++ src/main/java/game/MoveCalculator.java
@@ -17,15 +17,11 @@ public final class MoveCalculator {
      * @return All valid moves for this player
      */
     public static Set<Move> getValidMovesForPlayer(Game game, int playerNumber) {
-        return getValidMovesForPlayer(game, playerNumber, null);
-    }
-
-    public static Set<Move> getValidMovesForPlayer(Game game, int playerNumber, Set<Coordinates> area) {
 
         Set<Move> result = new HashSet<>();
 
         switch (game.getPhase()) {
-            case BUILD -> result.addAll(calculateAllColoringMoves(game, playerNumber, area));
+            case BUILD -> result.addAll(calculateAllColoringMoves(game, playerNumber));
             case BOMB -> result.addAll(getAllBombMoves(game, playerNumber));
             default -> throw new GamePhaseNotValidException(
                     "No valid game phase to calculate moves for");
@@ -34,7 +30,7 @@ public final class MoveCalculator {
         return result;
     }
 
-    private static Set<Move> calculateAllColoringMoves(Game game, int playerNumber, Set<Coordinates> area) {
+    private static Set<Move> calculateAllColoringMoves(Game game, int playerNumber) {
 
         Player player = game.getPlayer(playerNumber);
 
@@ -42,10 +38,6 @@ public final class MoveCalculator {
 
         for (Coordinates occupiedTile : game.getAllCoordinatesWhereTileIs(
                 player.getPlayerValue())) {
-
-            if(!area.contains(occupiedTile))
-                continue;
-
             if (game.getTile(occupiedTile) != player.getPlayerValue()) {
                 throw new AssertionError("Wrong coordinates in Player" + player + "'s List stones");
             }
@@ -63,10 +55,6 @@ public final class MoveCalculator {
         if (player.getOverwriteStones() > 0) {
             // Add overwrite moves on expansion tiles
             for (var coordinate : game.gameStats.getAllCoordinatesWhereTileIs(Tile.EXPANSION)) {
-
-                // TODO: ???
-                if(!area.contains(coordinate)) continue;
-
                 moves.add(new OverwriteMove(playerNumber, coordinate));
             }
         }
