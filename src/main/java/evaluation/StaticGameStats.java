a3605625b0ed50ecf76637a419850cc9f0aad475
Ludwig Schmidt <schmidtludwig8@gmail.com>
2024-06-16 18:15:51 +0200

    Add feature that calclculation of reachable tiles terminates earlier due to better performance

--- src/main/java/evaluation/StaticGameStats.java
+++ src/main/java/evaluation/StaticGameStats.java
@@ -7,6 +7,7 @@ import board.TileReader;
 import game.Game;
 import game.GamePhase;
 import move.Move;
+import move.OverwriteMove;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -38,13 +39,19 @@ public class StaticGameStats {
      */
     private final int width;
     /**
-     * Width of board
+     * Height of board
      */
     private final int height;
 
     private final int[][] tileRatings;
 
-    private short numberOfReachableTiles;
+    private final short potentialReachableTiles;
+
+    private short reachableTiles;
+    /**
+     * Constant that is used for in initializeReachableTiles
+     */
+    private final Boundaries noOverwriteTheMoveBefore = new Boundaries(0, 0);
 
     public StaticGameStats(Game initialGame, int initialPlayers, int initialOverwriteStones,
                            int initialBombs, int bombRadius) {
@@ -55,14 +62,15 @@ public class StaticGameStats {
         width = initialGame.getWidth();
         height = initialGame.getHeight();
         tileRatings = calculateTileRatings(initialGame);
-        numberOfReachableTiles = 0;
+        potentialReachableTiles = calculatePotentialReachableTiles(initialGame);
+        reachableTiles = 0;
     }
 
     private int[][] calculateTileRatings(Game game) {
         int[][] tileRatings = new int[height][width];
-        for(int y = 0; y < height; y++) {
-            for(int x = 0; x < width; x++) {
-                if(game.getTile(new Coordinates(x, y)) == Tile.WALL){
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                if (game.getTile(new Coordinates(x, y)) == Tile.WALL) {
                     tileRatings[y][x] = 0;
                     continue;
                 }
@@ -72,33 +80,72 @@ public class StaticGameStats {
         return tileRatings;
     }
 
+    private short calculatePotentialReachableTiles(Game initialGame) {
+        int allTiles = width * height;
+        int allWallTiles = initialGame.getAllCoordinatesWhereTileIs(Tile.WALL).size();
+        return (short) (allTiles - allWallTiles);
+    }
+
     public void initializeReachableTiles(Game initialGame) {
+        //long time = System.currentTimeMillis();
         Game purposeGame = initialGame.clone();
-        while(purposeGame.getPhase() != GamePhase.BOMB){
+
+        // Is used to check if only overwrite moves were played for a whole round
+        Boundaries boundaries = noOverwriteTheMoveBefore;
+
+        // System.currentTimeMillis() - time < 1000 &&
+        while (purposeGame.getPhase() == GamePhase.BUILD) {
             Set<Move> validMovesForCurrentPlayer = purposeGame.getRelevantMovesForCurrentPlayer();
             List<Move> ListOfRelevantMoves = new ArrayList<>(validMovesForCurrentPlayer);
+            if (ListOfRelevantMoves.get(0) instanceof OverwriteMove) {
+                int playerNumber = purposeGame.getCurrentPlayerNumber();
+
+                // Check if only overwrite moves were played for a whole round
+                if (!boundaries.equals(noOverwriteTheMoveBefore) && (playerNumber >= boundaries.lowerBoundary() ||
+                        playerNumber <= boundaries.upperBoundary())) {
+                    break;
+                }
+                else {
+                    boundaries = updateBoundaries(boundaries, playerNumber);
+                }
+            }
+            else {
+                boundaries = noOverwriteTheMoveBefore;
+            }
             int randomIndex = (int) (Math.random() * validMovesForCurrentPlayer.size());
-            if(!validMovesForCurrentPlayer.isEmpty()){
-                Move randomMove = ListOfRelevantMoves.get(randomIndex);
-                purposeGame.executeMove(randomMove);
-            }
-            else{
-                break;
-            }
+            Move randomMove = ListOfRelevantMoves.get(randomIndex);
+            purposeGame.executeMove(randomMove);
         }
+
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
-                if(purposeGame.getTile(new Coordinates(x,y)).isPlayer()){
-                    numberOfReachableTiles++;
+                if (purposeGame.getTile(new Coordinates(x, y)).isPlayer()) {
+                    reachableTiles++;
                 }
             }
         }
+        System.out.println("Potential reachable tiles: " + potentialReachableTiles);
         printAmountOfReachableTiles();
     }
 
-    public void printAmountOfReachableTiles(){
+    private Boundaries updateBoundaries(Boundaries boundaries, int playerNumber) {
+        if (boundaries.equals(noOverwriteTheMoveBefore)) {
+            return new Boundaries(getLowerBoundary(playerNumber), getUpperBoundary(playerNumber));
+        }
+        return new Boundaries(getLowerBoundary(playerNumber), boundaries.upperBoundary());
+    }
+
+    private int getLowerBoundary(int playerNumber) {
+        return (playerNumber - 2) % 8 + 1;
+    }
+
+    private int getUpperBoundary(int playerNumber) {
+        return playerNumber % 8 + 1;
+    }
+
+    public void printAmountOfReachableTiles() {
         System.out.println();
-        System.out.println("\nNumber of reachable tiles: " + numberOfReachableTiles);
+        System.out.println("\nNumber of reachable tiles: " + reachableTiles);
     }
 
     /**
@@ -107,23 +154,24 @@ public class StaticGameStats {
      * Such angles are valuable, as tiles can "attack" in the angle but can not be "attacked"
      * 1 bonuspoint for every angle, that fullfills the criterion
      * Rating for WALLS: 0
+     *
      * @param x X-coordinate
      * @param y Y-coordinate
      * @return Tile rating as an Integer
      */
 
-    private int calculateParicularTileRating(int x, int y, Game game){
+    private int calculateParicularTileRating(int x, int y, Game game) {
         int tileRating = 1;
-        Direction[] halfOfAllDirections = Arrays.copyOfRange(Direction.values(),0, 4);
-        for(Direction direction : halfOfAllDirections) {
+        Direction[] halfOfAllDirections = Arrays.copyOfRange(Direction.values(), 0, 4);
+        for (Direction direction : halfOfAllDirections) {
             TileReader tileReader = new TileReader(game, new Coordinates(x, y), direction);
             TileReader oppositeDirectionTileReader =
                     new TileReader(game, new Coordinates(x, y), direction.getOppositeDirection());
-            if(tileReader.hasNext() && !oppositeDirectionTileReader.hasNext()){
+            if (tileReader.hasNext() && !oppositeDirectionTileReader.hasNext()) {
                 tileRating++;
                 continue;
             }
-            if(!tileReader.hasNext() && oppositeDirectionTileReader.hasNext()){
+            if (!tileReader.hasNext() && oppositeDirectionTileReader.hasNext()) {
                 tileRating++;
             }
         }
@@ -151,5 +199,13 @@ public class StaticGameStats {
 
     public int[][] getTileRatings() {
         return tileRatings;
+    }
+
+    public short getReachableTiles() {
+        return reachableTiles;
+    }
+
+    public short getPotentialReachableTiles() {
+        return potentialReachableTiles;
     }
 }
