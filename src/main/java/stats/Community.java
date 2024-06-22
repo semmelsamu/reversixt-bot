86fa195c960be86643a77c4c3b1ba33c4314f192
Samuel Kroiß <semmikroiss@gmail.com>
2024-06-22 11:37:55 +0200

    Refactor PlayerTileAmountContainer to own file

--- src/main/java/stats/Community.java
+++ src/main/java/stats/Community.java
@@ -105,6 +105,13 @@ public class Community implements Cloneable {
         return -1;
     }
 
+    /*
+    |-----------------------------------------------------------------------------------------------
+    |
+    |   Overrides
+    |
+    |-----------------------------------------------------------------------------------------------
+    */
 
     @Override
     public boolean equals(Object o) {
@@ -156,61 +163,4 @@ public class Community implements Cloneable {
                 "  playerTileAmountContainers=" + playerTileAmountsString + "\n" + '}';
     }
 
-    static class PlayerTileAmountContainer implements Cloneable {
-        private final Tile player;
-        private int tileAmount;
-
-        public PlayerTileAmountContainer(Tile player, int tileAmount) {
-            this.player = player;
-            this.tileAmount = tileAmount;
-        }
-
-        public Tile getPlayer() {
-            return player;
-        }
-
-        public int getTileAmount() {
-            return tileAmount;
-        }
-
-        public void incrementTileAmount() {
-            tileAmount++;
-        }
-
-        public void decrementTileAmount() {
-            tileAmount--;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (o == null || getClass() != o.getClass()) {
-                return false;
-            }
-            PlayerTileAmountContainer that = (PlayerTileAmountContainer) o;
-            return tileAmount == that.tileAmount && Objects.equals(player, that.player);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(player, tileAmount);
-        }
-
-        @Override
-        protected PlayerTileAmountContainer clone() throws CloneNotSupportedException {
-            try {
-                return (PlayerTileAmountContainer) super.clone();
-            } catch (CloneNotSupportedException e) {
-                throw new AssertionError();
-            }
-        }
-
-        @Override
-        public String toString() {
-            return "PlayerTileAmountContainer {\n" + "    player=" + player + ",\n" +
-                    "    tileAmount=" + tileAmount + "\n" + "  }";
-        }
-    }
 }
