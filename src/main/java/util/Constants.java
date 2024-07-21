b3b57424d0509d56f7ee1fd1bd210d3e53535c4a
Samuel Kroiß <semmikroiss@gmail.com>
2024-07-21 17:41:35 +0200

    Remove Community constants

--- src/main/java/util/Constants.java
+++ src/main/java/util/Constants.java
@@ -10,15 +10,4 @@ public class Constants {
      */
     public static int TIME_BUFFER = 500;
 
-    /**
-     * The radius in which neighbouring Communitys will merge.
-     */
-    public static int COMMUNITY_MERGE_RADIUS = 3;
-
-    /**
-     * The factor used to calculate the penalty for a "dead" Community. This constant is multiplied
-     * by the wasted potential of the Community to determine the final penalty value.
-     */
-    public static int DEAD_COMMUNITY_PUNISHMENT_FACTOR = 1;
-
 }
